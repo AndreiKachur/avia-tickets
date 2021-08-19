9af356bb4f4a01a03554d6c8211dbcde7001d2af
Andrei <underfil@mail.ru>
2021-08-19 12:43:28 +0300

    Side-bar markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='layout'>
+      <aside className='side-bar'>
+
+        <section className='side-bar__section'>
+          <h3>Сортировать</h3>
+          <div>
+            <input type='radio' name='increasing' />
+            <label for='increasing'>- по возрастанию цены</label>
+          </div>
+          <div>
+            <input type='radio' name='decreasing' />
+            <label for='decreasing'>- по убыванию цены</label>
+          </div>
+          <div>
+            <input type='radio' name='time' />
+            <label for='time'>- по времени в пути</label>
+          </div>
+        </section>
+
+        <section className='side-bar__section'>
+          <h3>Фильтровать</h3>
+          <div>
+            <input type='checkbox' name='with-changing' />
+            <label for='with-changeing'>- 1 пересадка</label>
+          </div>
+          <div>
+            <input type='checkbox' name='without-changing' />
+            <label for='without-changing'>- без пересадок</label>
+          </div>
+        </section>
+
+        <section className='side-bar__section'>
+          <h3>Цена</h3>
+          <div className='side-bar__section__price'>
+            <label for='from'>От</label>
+            <input type='number' name='from' className='side-bar__section__input-text' />
+          </div>
+          <div className='side-bar__section__price side-bar__section_zero-margin'>
+            <label for='to'>До</label>
+            <input type='number' name='to' className='side-bar__section__input-text' />
+          </div>
+        </section>
+
+        <section className='side-bar__section'>
+          <h3>Авиакомпании</h3>
+          <div>
+            <input type='checkbox' name='with-changing' />
+            <label for='with-changeing'>- LOT Polish Airlines от 20000 р.</label>
+          </div>
+          <div>
+            <input type='checkbox' name='without-changing' />
+            <label for='without-changing'>- Аэрофлот рос... от 30000 р.</label>
+          </div>
+        </section>
+
+      </aside>
+      <main className='main'>
+        Hello world
+    </main>
     </div>
   );
 }
