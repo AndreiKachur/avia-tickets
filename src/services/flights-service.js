c87fb6fc60c059ea902dfb1f8dd5f7c0d6754458
Andrei <underfil@mail.ru>
2021-08-23 00:05:30 +0300

    change filter, add questionary

--- src/services/flights-service.js
+++ src/services/flights-service.js
@@ -13,20 +13,43 @@ export default class FlightsService {
     }
     getTransfer(transfer) {
         switch (transfer) {
-            case 'one': return `&flight.legs.0.segments.length=2&&flight.legs.1.segments.length=2`
-            case 'none': return `&flight.legs.0.segments.length=1&&flight.legs.1.segments.length=1`
+            case 'one': return `?flight.legs.0.segments.length=1&flight.legs.1.segments.length=2`
+            case 'onemore': return `?flight.legs.0.segments.length=2&flight.legs.1.segments.length=1`
+            case 'none': return `&flight.legs.0.segments.length=1&flight.legs.1.segments.length=1`
             default: return ''
         }
     }
+    filterCompanies = companies => companies
+        .reduce((res, company) => res + `&flight.carrier.caption=${company}`, '')
+
     async getFiltered(carriers, carriersChecked, transfer) {
-        let filtered = this.getTransfer(transfer)
-        let companies = []
+        let filtered, filtered2, companies = []
+
         carriersChecked.forEach((el, i) => {
             if (el) companies.push(carriers[i])
         })
-        if (companies) {
-            filtered = filtered + companies
-                .reduce((res, company) => res + `&flight.carrier.caption=${company}`, '')
+        if (transfer === 'none') {
+            filtered = this.getTransfer(transfer)
+        }
+        if (transfer === 'one') {
+
+            if (companies.length > 0) {
+                filtered = this.getTransfer('one') + this.filterCompanies(companies)
+                filtered2 = this.getTransfer('onemore') + this.filterCompanies(companies)
+
+            } else {
+                filtered = this.getTransfer('one')
+                filtered2 = this.getTransfer('onemore')
+            }
+            const temp1 = await this.getResourses(`/flights${filtered}`)
+            const temp2 = await this.getResourses(`/flights${filtered2}`)
+
+            return filtered = [...temp1, ...temp2]
+
+        }
+
+        if (companies.length > 0) {
+            filtered = filtered + this.filterCompanies(companies)
         }
         filtered = filtered ? '?' + filtered : '/'
 
