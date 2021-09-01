c7307bc8b5a20d02138703e5205e6c7302b3028a
Andrei <underfil@mail.ru>
2021-09-01 14:02:27 +0300

    minor refactor

--- src/services/flights-service.js
+++ src/services/flights-service.js
@@ -8,9 +8,6 @@ export default class FlightsService {
         if (!res.ok) throw new Error(`Could not fetch ${url}, status ${res.status}`)
         return await res.json()
     }
-    async getFlightsItems() {
-        return await this.getResourses(`/flights/`)
-    }
     getTransfer(transfer) {
         switch (transfer) {
             case 'one': return `?flight.legs.0.segments.length=1&flight.legs.1.segments.length=2`
@@ -36,7 +33,6 @@ export default class FlightsService {
             if (companies.length > 0) {
                 filtered = this.getTransfer('one') + this.filterCompanies(companies)
                 filtered2 = this.getTransfer('onemore') + this.filterCompanies(companies)
-
             } else {
                 filtered = this.getTransfer('one')
                 filtered2 = this.getTransfer('onemore')
@@ -45,7 +41,6 @@ export default class FlightsService {
             const temp2 = await this.getResourses(`/flights${filtered2}`)
 
             return filtered = [...temp1, ...temp2]
-
         }
 
         if (companies.length > 0) {
